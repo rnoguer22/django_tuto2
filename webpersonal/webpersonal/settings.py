9d6e749805be365d8a6c6c9de83ba2ff484da429
rnoguer22 <rnogugon@myuax.com>
2023-11-04 00:30:23 +0100

    templates

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core", #Añadimos la app core
 ]
 
 MIDDLEWARE = [
--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("admin/", admin.site.urls),
+    path("about-me/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("portfolio/", views.portfolio, name="portfolio"),
 ]
