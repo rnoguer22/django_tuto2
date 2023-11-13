f9355891ad662c544f4372a98df1c313f88500e0
rnoguer22 <rnogugon@myuax.com>
2023-11-13 23:57:50 +0100

    Implementacion arduino para cambiar de pagina

---
+++ arduino/arduino.ino
@@ -0,0 +1,42 @@
+const int numLEDs = 4; //Numero de leds que vamos a poner
+const int ledPins[numLEDs] = {32, 40, 44, 48};
+int option;
+
+void setup() {
+  Serial.begin(9600); //Inicializamos el puerto Serial
+  //Configuramos los leds como salida
+  for (int i=0; i<numLEDs; i++){
+    pinMode(ledPins[i], OUTPUT);
+  }
+}
+
+void loop() {
+  //Si escribimos algo en el puerto serial, Serial.available > 0
+  if (Serial.available() >= 1){ 
+    option = Serial.read(); //Leemos lo que hay en el puerto serial
+    if (option == '1'){
+      digitalWrite(ledPins[0], HIGH);
+      digitalWrite(ledPins[1], LOW);      
+      digitalWrite(ledPins[2], LOW);
+      digitalWrite(ledPins[3], LOW);
+    }
+    if (option == '2'){
+      digitalWrite(ledPins[0], LOW);
+      digitalWrite(ledPins[1], HIGH);      
+      digitalWrite(ledPins[2], LOW);
+      digitalWrite(ledPins[3], LOW);
+    }
+    if (option == '3'){
+      digitalWrite(ledPins[0], LOW);
+      digitalWrite(ledPins[1], LOW);      
+      digitalWrite(ledPins[2], HIGH);
+      digitalWrite(ledPins[3], LOW);
+    }
+    if (option == '4'){
+      digitalWrite(ledPins[0], LOW);
+      digitalWrite(ledPins[1], LOW);      
+      digitalWrite(ledPins[2], LOW);
+      digitalWrite(ledPins[3], HIGH);
+    }
+  }
+}
--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,16 +1,60 @@
 from django.shortcuts import render, HttpResponse
+import serial
+import time
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, "core/home.html")
+    try:
+        ser = serial.Serial('COM9', 9600)  # Ajusta el puerto según tu configuración
+    except:
+        print("No se pudo abrir el puerto serial")
+    else:
+        time.sleep(1)
+        ser.write(b'1')  # Envía un byte para encender la luz
+        print('portada')
+        ser.close()
+    finally:
+        return render(request, "core/home.html")
+    
 
 def about(request):
-    return render(request, "core/about.html")
+    try:
+        ser = serial.Serial('COM9', 9600)  # Ajusta el puerto según tu configuración
+    except:
+        print("No se pudo abrir el puerto serial")
+    else:
+        time.sleep(1)
+        ser.write(b'2')  # Envía un byte para encender la luz
+        print('acerca')
+        ser.close()
+    finally:
+        return render(request, "core/about.html")
+    
 
 def contact(request):
-    return render(request, "core/contact.html")
+    try:
+        ser = serial.Serial('COM9', 9600)  # Ajusta el puerto según tu configuración
+    except:
+        print("No se pudo abrir el puerto serial")
+    else:
+        time.sleep(1)
+        ser.write(b'3')  # Envía un byte para encender la luz
+        print('contacto')
+        ser.close()
+    finally:
+        return render(request, "core/contact.html")
 
 def portfolio(request):
-    return render(request, "core/portfolio.html")
+    try:
+        ser = serial.Serial('COM9', 9600)  # Ajusta el puerto según tu configuración
+    except:
+        print("No se pudo abrir el puerto serial")
+    else:
+        time.sleep(1)
+        ser.write(b'4')  # Envía un byte para encender la luz
+        print('portafolio')
+        ser.close()
+    finally:
+        return render(request, "core/portfolio.html")
