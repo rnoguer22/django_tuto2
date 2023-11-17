f965f3f23701953027b9e15a8d4ab10241a59b67
rnoguer22 <rnogugon@myuax.com>
2023-11-17 17:44:28 +0100

    cambio en el arduino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,5 +1,5 @@
-const int numLEDs = 4; //Numero de leds que vamos a poner
-const int ledPins[numLEDs] = {32, 40, 44, 48};
+const int numLEDs = 3; //Numero de leds que vamos a poner
+const int ledPins[numLEDs] = {32, 40, 48};
 int option;
 
 void setup() {
@@ -18,25 +18,16 @@ void loop() {
       digitalWrite(ledPins[0], HIGH);
       digitalWrite(ledPins[1], LOW);      
       digitalWrite(ledPins[2], LOW);
-      digitalWrite(ledPins[3], LOW);
     }
     if (option == '2'){
       digitalWrite(ledPins[0], LOW);
       digitalWrite(ledPins[1], HIGH);      
       digitalWrite(ledPins[2], LOW);
-      digitalWrite(ledPins[3], LOW);
     }
     if (option == '3'){
       digitalWrite(ledPins[0], LOW);
       digitalWrite(ledPins[1], LOW);      
       digitalWrite(ledPins[2], HIGH);
-      digitalWrite(ledPins[3], LOW);
-    }
-    if (option == '4'){
-      digitalWrite(ledPins[0], LOW);
-      digitalWrite(ledPins[1], LOW);      
-      digitalWrite(ledPins[2], LOW);
-      digitalWrite(ledPins[3], HIGH);
     }
   }
 }
